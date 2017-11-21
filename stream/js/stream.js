f24c4541532627abc8942a51c89c2a5ea23fff0e
Clifton Rapier <c.rapier@hotmail.com>
2017-11-20 19:44:04 -0500

    Change plugin for livestream

--- stream/js/stream.js
+++ stream/js/stream.js
@@ -6,21 +6,21 @@ $(document).ready(function(){
     flowplayer("#player", "http://releases.flowplayer.org/swf/flowplayer-3.2.18.swf", {
     
         clip: {
-            url: 'mp4:vod/demo.flowplayer/buffalo_soldiers.mp4',
-            scaling: 'fit',
+            url: 'emerzil',
+            live: true,
             // configure clip to use hddn as our provider, referring to our rtmp plugin
-            provider: 'hddn'
+            provider: 'influxis'
         },
     
         // streaming plugins are configured under the plugins node
         plugins: {
     
             // here is our rtmp plugin configuration
-            hddn: {
+            influxis: {
                 url: "flowplayer.rtmp-3.2.13.swf",
-    
+     
                 // netConnectionUrl defines where the streams are found
-                netConnectionUrl: 'rtmp://10.0.0.175:1935/live/emerzil'
+                netConnectionUrl: 'rtmp://10.0.0.175/live'
             }
         },
         canvas: {
