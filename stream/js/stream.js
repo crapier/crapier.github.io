40eea1a1308fbe8541831f0f7d226f90332ee7c7
Clifton Rapier <c.rapier@hotmail.com>
2017-11-20 19:57:05 -0500

    Plugin Changes

--- stream/js/stream.js
+++ stream/js/stream.js
@@ -3,28 +3,20 @@
 $(document).ready(function(){
 
     // install flowplayer into selected container
-    flowplayer("#player", "http://releases.flowplayer.org/swf/flowplayer-3.2.18.swf", {
+    flowplayer("player", "http://releases.flowplayer.org/swf/flowplayer-3.2.18.swf", {
     
         clip: {
             url: 'emerzil',
-            live: true,
-            // configure clip to use hddn as our provider, referring to our rtmp plugin
-            provider: 'influxis'
+            provider: 'rtmp',
+            live: true
         },
-    
-        // streaming plugins are configured under the plugins node
         plugins: {
-    
-            // here is our rtmp plugin configuration
-            influxis: {
+            rtmp: {
                 url: "flowplayer.rtmp-3.2.13.swf",
      
-                // netConnectionUrl defines where the streams are found
-                netConnectionUrl: 'rtmp://10.0.0.175/live'
+                netConnectionUrl: 'rtmp://10.0.0.175:1935/live',
+                subscribe: true
             }
-        },
-        canvas: {
-            backgroundGradient: 'none'
         }
     });
 });
