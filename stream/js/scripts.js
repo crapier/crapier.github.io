b359f92719937faa8430b26ef2c09501d31ea5f6
Clifton Rapier <c.rapier@hotmail.com>
2017-11-29 21:27:27 -0500

    Visual Tweaks, Allow different Streams
    Video fills whole page
    local swf
    stream key by url parameter (default emerzil)

--- stream/js/scripts.js
+++ stream/js/scripts.js
@@ -1,6 +1,26 @@
 
 $(document).ready(function(){
-    var stream = videojs('video-stream', {techOrder: ['flash']});
-    stream.src({type: "rtmp/mp4", src: "rtmp://emeraldserver.chickenkiller.com/live/emerzil"});
+    var parameters = function() {
+        var parameters = document.location.href;
+        if (parameters.indexOf('?') < 0) {
+            return {};
+        }
+        parameters = parameters.substring(parameters.indexOf('?') + 1);
+        parameters = parameters.split('&');
+        parameters.forEach(function(element, index, params) {
+            params[index] = element.split('=');
+        });
+        var parametersObj = {};
+        parameters.forEach(function(element) {
+            if(element.length == 2) {
+                parametersObj[element[0]] = element[1];
+            }
+        });
+        return parametersObj;
+    }();
+    var streamkey = parameters['id'] ? parameters['id'] : 'emerzil';
+
+    var stream = videojs('video-stream', {techOrder: ['flash'], flash: {swf: "video-js.swf"}});
+    stream.src({type: "rtmp/mp4", src: "rtmp://emerzil.chickenkiller.com/live/" + streamkey });
 });
-            
+
