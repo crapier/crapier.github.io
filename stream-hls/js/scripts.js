298142c7c1d15cb25059fbc134fe89bbdbe5b6c6
Clifton Rapier <c.rapier@hotmail.com>
2017-12-02 21:02:48 -0500

    Add Seeking to HLS Playback
    HLS only seems to work from server thats it hosting the hls stream (CORS issue)

--- stream-hls/js/scripts.js
+++ stream-hls/js/scripts.js
@@ -24,5 +24,59 @@ $(document).ready(function(){
 
     stream = videojs('video-stream');
     stream.src({type: "application/x-mpegURL", src: "http://emerzil.chickenkiller.com:8080/hls/" + streamkey + ".m3u8" });
+    
+    
+    // Custom seek controls/buttons
+    function fullRewind() {
+        if(!stream.seeking()) {
+            stream.currentTime(stream.seekable().start(0) + 15);
+        }
+    }
+    
+    function fullForward() {
+        if(!stream.seeking()) {
+            stream.currentTime(stream.seekable().end(0));
+        }
+    }
+    
+    function stepRewind() {
+        if(!stream.seeking()) {
+            var seekTime = stream.currentTime() - 15;
+            if (seekTime < stream.seekable().start(0) + 15) {
+                seekTime = stream.seekable().start(0) + 15;
+            }
+            stream.currentTime(seekTime);
+        }
+    }
+    
+    function stepForward() {
+        if(!stream.seeking()) {
+            var seekTime = stream.currentTime() + 15;
+            if (seekTime > stream.seekable().end(0)) {
+                seekTime = stream.seekable().end(0);
+            }
+            stream.currentTime(seekTime);
+        }
+    }
+    
+    $(".vjs-live-display").click(fullForward);
+    
+    $('<div/>', {
+        style: 'padding-left: 15px',
+        text: '<<<<Rewind',
+        click: fullRewind
+    }).appendTo('.vjs-live-control.vjs-control');
+    
+    $('<div/>', {
+        style: 'padding-left: 15px',
+        text: '<<Step Back',
+        click: stepRewind
+    }).appendTo('.vjs-live-control.vjs-control');
+    
+    $('<div/>', {
+        style: 'padding-left: 15px',
+        text: 'Step Forward>>',
+        click: stepForward
+    }).appendTo('.vjs-live-control.vjs-control');
 });
 
